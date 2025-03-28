9cbabce4a884f3163fabd6362b3dccd3e718772f
IsaacScheff <isaac1996@comcast.net>
2025-03-28 17:18:15 -0400

    Added player wizard and shield, can now block cowboy shots with shield

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -3,6 +3,9 @@ import Phaser from 'phaser';
 class MainScene extends Phaser.Scene {
   constructor() {
     super({ key: 'MainScene' });
+    this.shieldActive = false;
+    this.nextShotTime = 0;
+    this.shotFrameToCheck = 9;
   }
 
   preload() {
@@ -11,26 +14,92 @@ class MainScene extends Phaser.Scene {
       frameWidth: 48, 
       frameHeight: 48 
     });
+    this.load.image('wizardOne', 'assets/images/WizardOne.png');
+    this.load.image('wizardShield', 'assets/images/WizShieldOne.png');
   }
 
   create() {
     this.add.image(0, 0, 'background').setOrigin(0, 0);
     this.cowboy = this.add.sprite(190, 160, 'cowboy'); 
-    
+    this.wizard = this.add.sprite(66, 160, 'wizardOne');
+
     this.anims.create({
       key: 'idle',
-      frames: this.anims.generateFrameNumbers('cowboy', {
-        start: 0,
-        end: 6
-      }),
+      frames: this.anims.generateFrameNumbers('cowboy', { start: 0, end: 6 }),
       frameRate: 10,
       repeat: -1
     });
     
+    this.anims.create({
+      key: 'quickDraw',
+      frames: this.anims.generateFrameNumbers('cowboy', { start: 14, end: 31 }),
+      frameRate: 20,
+      repeat: 0
+    });
+    
     this.cowboy.play('idle');
+    this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    
+    this.shield = this.add.image(this.wizard.x + 20, this.wizard.y, 'wizardShield')
+      .setVisible(false)
+      .setDepth(1);
+    
+    this.scheduleRandomShot();
   }
 
   update() {
+    const now = this.time.now;
+    
+    if (now >= this.nextShotTime && this.cowboy.anims.currentAnim?.key === 'idle') {
+      this.playQuickDraw();
+      this.scheduleRandomShot();
+    }
+    
+    if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
+      this.activateShield();
+    }
+  }
+  
+  scheduleRandomShot() {
+    const delay = Phaser.Math.Between(3000, 6000);
+    this.nextShotTime = this.time.now + delay;
+  }
+  
+  activateShield() {
+    if (!this.shieldActive) {
+      this.shieldActive = true;
+      this.shield.setVisible(true);
+      
+      this.time.delayedCall(1000, () => {
+        this.shield.setVisible(false);
+        this.shieldActive = false;
+      });
+    }
+  }
+  
+  playQuickDraw() {
+    if (this.cowboy.anims.currentAnim?.key !== 'quickDraw') {
+      this.cowboy.play('quickDraw');
+      
+      this.cowboy.on('animationupdate', (animation, frame) => {
+        if (frame.index === this.shotFrameToCheck) {
+          this.checkShotResult();
+          this.cowboy.off('animationupdate');
+        }
+      });
+      
+      this.cowboy.once('animationcomplete', () => {
+        this.cowboy.play('idle');
+      });
+    }
+  }
+  
+  checkShotResult() {
+    if (this.shieldActive) {
+      console.log("blocked!");
+    } else {
+      console.log("hit!");
+    }
   }
 }
 
