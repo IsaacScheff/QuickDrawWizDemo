6375268aa66e744e4b8927e87871c2b7fa51f576
IsaacScheff <isaac1996@comcast.net>
2025-03-30 23:14:00 -0400

    Added main menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import Phaser from 'phaser';
+import MainMenuScene from './scenes/MainMenuScene.js';
 import ShowdownScene from './scenes/ShowdownScene.js';
 
 const config = {
   type: Phaser.AUTO,
   width: 256,
   height: 224,
-  scene: [ShowdownScene],
+  scene: [MainMenuScene, ShowdownScene],
   zoom: 3
 };
 
---
+++ src/scenes/MainMenuScene.js
@@ -0,0 +1,92 @@
+export default class MainMenuScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MainMenuScene' });
+    }
+
+    create() {
+        this.add.rectangle(0, 0, this.cameras.main.width, this.cameras.main.height, 0x222034)
+            .setOrigin(0);
+        
+        const tutorialButton = this.add.rectangle(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY - 60,
+            300,
+            60,
+            0x4a4a8a
+        ).setInteractive();
+        
+        const tutorialText = this.add.text(
+            tutorialButton.x,
+            tutorialButton.y,
+            'TUTORIAL',
+            { 
+                fontSize: '32px',
+                color: '#FFFFFF',
+                fontStyle: 'bold'
+            }
+        ).setOrigin(0.5);
+        
+        const showdownButton = this.add.rectangle(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY + 60,
+            300,
+            60,
+            0x8a4a4a
+        ).setInteractive();
+        
+        const showdownText = this.add.text(
+            showdownButton.x,
+            showdownButton.y,
+            'SHOWDOWN',
+            { 
+                fontSize: '32px',
+                color: '#FFFFFF',
+                fontStyle: 'bold'
+            }
+        ).setOrigin(0.5);
+        
+        tutorialButton.on('pointerover', () => {
+            tutorialButton.setFillStyle(0x6a6aaa);
+        });
+        
+        tutorialButton.on('pointerout', () => {
+            tutorialButton.setFillStyle(0x4a4a8a);
+        });
+        
+        showdownButton.on('pointerover', () => {
+            showdownButton.setFillStyle(0xaa6a6a);
+        });
+        
+        showdownButton.on('pointerout', () => {
+            showdownButton.setFillStyle(0x8a4a4a);
+        });
+        
+        tutorialButton.on('pointerdown', () => {
+            tutorialButton.setFillStyle(0x3a3a7a);
+        });
+        
+        tutorialButton.on('pointerup', () => {
+            tutorialButton.setFillStyle(0x4a4a8a);
+            this.scene.start('TutorialScene');
+        });
+        
+        showdownButton.on('pointerdown', () => {
+            showdownButton.setFillStyle(0x7a3a3a);
+        });
+        
+        showdownButton.on('pointerup', () => {
+            showdownButton.setFillStyle(0x8a4a4a);
+            this.scene.start('ShowdownScene');
+        });
+
+        this.add.text(
+            this.cameras.main.centerX,
+            this.cameras.main.height - 30,
+            'Click to select mode',
+            { 
+                fontSize: '16px',
+                color: '#AAAAAA'
+            }
+        ).setOrigin(0.5);
+    }
+}
